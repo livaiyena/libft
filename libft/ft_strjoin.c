c8af9a70b394d5da5d97d384ea97965b2d640a75
Kerem Apaydın <apaydinkerem18@gmail.com>
2026-01-06 12:27:46 +0300

    strjoin allocation control fixed

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -9,6 +9,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = ft_strlen(s1);
 	j = ft_strlen(s2);
 	str = (char *)malloc(sizeof(char) * (i + j + 1));
+	if (!str)
+		return (NULL);
 	ft_memcpy(str, s1, i);
 	ft_memcpy(str + i - 1, s2, j);
 	str[i + j - 1] = '\0';
