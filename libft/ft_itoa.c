5e36ef54c70d75c330a8ee13fbf7f746efcbdc4d
Kerem Apaydın <apaydinkerem18@gmail.com>
2026-01-07 17:09:09 +0300

    added itoa striteri strmapi and completed part 2

---
+++ libft/ft_itoa.c
@@ -0,0 +1,43 @@
+#include "libft.h"
+
+static size_t	get_len(long n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	size_t	len;
+	long	nbr;
+
+	nbr = n;
+	len = get_len(nbr);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (nbr == 0)
+		str[0] = '0';
+	if (nbr < 0)
+	{
+		str[0] = '-';
+		nbr = -nbr;
+	}
+	while (nbr != 0)
+	{
+		str[--len] = (nbr % 10) + '0';
+		nbr /= 10;
+	}
+	return (str);
+}
